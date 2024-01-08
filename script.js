6ce431cd47685206a29bda4197358011d6da960d
anggahalim <155900734+anggahalim@users.noreply.github.com>
2024-01-08 09:05:44 +0700

    script.js

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const urlParams = new URLSearchParams(window.location.search);
   const nama = urlParams.get('nama');
   const seating = urlParams.get('seating');
-  document.getElementById('greeting').innerText = `Halo ${nama}, tempat duduk kamu berada di ${seating}.`;
+  
+  if (nama && seating) {
+    document.getElementById('greeting').innerText = `Halo ${nama}, tempat duduk kamu berada di ${seating}.`;
+  } else {
+    document.getElementById('greeting').innerText = 'QR code tidak valid.';
+  }
 });
